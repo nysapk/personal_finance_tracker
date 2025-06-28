68e22e72869828971406cc768a6ad48f7b916639
nysapk <kumar753@purdue.edu>
2025-06-28 15:36:22 -0700

    updated expense data table

---
+++ src/app/expenses/data-table.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import {
+	type ColumnDef,
+	flexRender,
+	getCoreRowModel,
+	useReactTable,
+} from "@tanstack/react-table";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "~/components/ui/table";
+
+interface DataTableProps<TData, TValue> {
+	columns: ColumnDef<TData, TValue>[];
+	data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+	columns,
+	data,
+}: DataTableProps<TData, TValue>) {
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	});
+
+	return (
+		<div className="rounded-md border overflow-auto">
+			<Table className="w-full table-fixed">
+				<TableHeader>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<TableRow key={headerGroup.id}>
+							{headerGroup.headers.map((header, index) => (
+								<TableHead
+									key={header.id}
+									className={`px-4 py-2 text-center bg-white ${index < headerGroup.headers.length - 1 ? "border-r" : ""} border-gray-300`}
+								>
+									{header.isPlaceholder
+										? null
+										: flexRender(
+												header.column.columnDef.header,
+												header.getContext(),
+											)}
+								</TableHead>
+							))}
+						</TableRow>
+					))}
+				</TableHeader>
+				<TableBody>
+					{table.getRowModel().rows.length ? (
+						table.getRowModel().rows.map((row) => (
+							<TableRow key={row.id} className="w-full">
+								{row.getVisibleCells().map((cell, index) => (
+									<TableCell
+										key={cell.id}
+										className={`px-4 py-2 text-center whitespace-nowrap overflow-hidden text-ellipsis ${index < row.getVisibleCells().length - 1 ? "border-r" : ""} border-gray-300`}
+									>
+										{flexRender(cell.column.columnDef.cell, cell.getContext())}
+									</TableCell>
+								))}
+							</TableRow>
+						))
+					) : (
+						<TableRow>
+							<TableCell colSpan={columns.length} className="h-24 text-center">
+								No results.
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
+		</div>
+	);
+}
