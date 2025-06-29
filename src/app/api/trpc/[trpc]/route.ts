1ee30d45ac21adb4a9359f3a29a168f1ff642b0f
nysapk <kumar753@purdue.edu>
2025-06-29 15:29:37 -0700

    fixed trpc route errors

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { type NextRequest } from "next/server";
 
@@ -9,6 +10,7 @@ import { createTRPCContext } from "~/server/api/trpc";
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a HTTP request (e.g. when you make requests from Client Components).
  */
+
 const createContext = async (req: NextRequest) => {
   return createTRPCContext({
     headers: req.headers,
@@ -23,7 +25,7 @@ const handler = (req: NextRequest) =>
     createContext: () => createContext(req),
     onError:
       env.NODE_ENV === "development"
-        ? ({ path, error }) => {
+        ? ({ path, error } : { path?: string; error: TRPCError }) => {
             console.error(
               `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
             );
