6487c5fe609e8e8ca2e653c888f20588154a6ebf
nysapk <kumar753@purdue.edu>
2025-06-29 16:33:00 -0700

    updated trpc and auth routes

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,21 +1,22 @@
-import { TRPCError } from "@trpc/server";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { type NextRequest } from "next/server";
+
 
 import { env } from "~/env";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
+
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a HTTP request (e.g. when you make requests from Client Components).
  */
-
 const createContext = async (req: NextRequest) => {
   return createTRPCContext({
     headers: req.headers,
   });
 };
+
 
 const handler = (req: NextRequest) =>
   fetchRequestHandler({
@@ -25,12 +26,16 @@ const handler = (req: NextRequest) =>
     createContext: () => createContext(req),
     onError:
       env.NODE_ENV === "development"
-        ? ({ path, error } : { path?: string; error: TRPCError }) => {
+        ? ({ path, error }) => {
             console.error(
-              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
             );
           }
         : undefined,
   });
 
+
 export { handler as GET, handler as POST };
+
+
+
